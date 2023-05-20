5998e15a5c8f52ff984e0a51f615e0da93a63d4b
panchocandioti <panchocandioti@gmail.com>
2023-05-20 12:42:50 -0300

    Mofificación de promedio3.js

--- promedio3.js
+++ promedio3.js
@@ -1,5 +1,15 @@
-let num1 = -1.2;
-let num2 = 5.3;
-let num3 = -3.3;
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
 let promedio3 = (num1 + num2 + num3) / 3;
 console.log(promedio3);
+
+num1 = -2;
+num2 = -3;
+num3 = -8;
+promedio3 = (num1 + num2 + num3) / 3;
+
+num1 = -1.2;
+num2 = 5.3;
+num3 = -3.3;
+promedio3 = (num1 + num2 + num3) / 3;
