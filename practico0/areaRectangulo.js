f0803a0727fe38dc13c37f8af061b2c9645cd34f
panchocandioti <panchocandioti@gmail.com>
2023-05-28 10:10:47 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,11 @@
+//TPN0 - Ejercicio 2 
+//Definicion de las variables con los primeros valores
+let base = 5;
+let altura = 3;
+let area = base * altura;
+console.log(area);
+//Nuevo cálculo con otros valores
+base = 12.5;
+altura = 5.3;
+area = base * altura;
+console.log(area);
