cab15f0e114c587d7b3a9d14d7ed0bf16313e063
panchocandioti <panchocandioti@gmail.com>
2023-05-28 10:05:18 -0300

    Agrego archivos resta.js y multiplicacion.js

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,6 @@
+//TPN0 - Ejercicio 1b 
+let factor1 = 2;
+let factor2 = 17;
+let factor3 = 6;
+let multiplicacion = factor1 * factor2 * factor3;
+console.log(multiplicacion);
---
+++ practico0/resta.js
@@ -0,0 +1,5 @@
+//TPN0 - Ejercicio 1a 
+let minuendo = 10;
+let sustraendo = 2;
+let resta = minuendo - sustraendo;
+console.log(resta);
