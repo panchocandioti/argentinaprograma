d829da4de5b0c9979787251d840dae856c5355d1
panchocandioti <panchocandioti@gmail.com>
2023-06-08 21:09:11 -0300

    Agrego Ejercicios 2 y 3 del TPN1

---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,11 @@
+let var1 = 12;
+let var2 = 10;
+let var3 = 24;
+let var4 = 22;
+let suma = var1 + var2;
+let resta = var3 - var4;
+let resultadoFinal = suma * resta;
+let resultadoFinalModulo = resultadoFinal % 2;
+let esImpar = Boolean(resultadoFinalModulo);
+let esPar = !esImpar;
+console.log("Mis variables iniciales fueron: %i, %i, %i, %i. La respuesta a verificar si el resultado final es par es: %s", var1, var2, var3, var4, esPar);
