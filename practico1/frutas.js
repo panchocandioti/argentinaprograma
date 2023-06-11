85485a0ed7d50c66a4db478c06b797f7494f3748
panchocandioti <panchocandioti@gmail.com>
2023-06-11 20:22:41 -0300

    Agrego Ejercicio 5 del TPN1

---
+++ practico1/frutas.js
@@ -0,0 +1,10 @@
+let frutas = [];
+frutas.push("manzana");
+frutas.push("pera");
+frutas.push("naranja");
+frutas.push("mandarina");
+frutas.push("ciruela");
+let ultima = frutas[frutas.length-1]
+console.log("La ultima fruta agregada fue: %s", ultima);
+let primera = frutas[0];
+console.log("La primera fruta agregada fue: %s", primera);
