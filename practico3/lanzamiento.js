51d8dbbb0e8bb9528e822e543fe43ca2028a7370
panchocandioti <panchocandioti@gmail.com>
2023-06-25 10:15:39 -0300

    Agrego Cuenta Regresiva del TPN3

---
+++ practico3/lanzamiento.js
@@ -0,0 +1,12 @@
+for (i = 10; i >= 0; i--) {
+    switch (i) {
+        case 10:
+            console.log("Cuenta regresiva 10");
+            break;
+        case 0:
+            console.log("Lanzamiento!");
+            break;
+        default:
+            console.log(i);
+    }
+}
